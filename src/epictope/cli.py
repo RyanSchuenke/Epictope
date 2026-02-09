6a444b24be2d2e671af706e7dd6f2d699c0871e3
RyanSchuenke <ryan.schuenke17@gmail.com>
2026-02-09 14:36:37 -0600

    add more detailed help messages for cli

--- src/epictope/cli.py
+++ src/epictope/cli.py
@@ -1,10 +1,19 @@
 import argparse
+import sys
+
+class EpictopeParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
 def main() -> None:
     """
     Command line interface for running epictope
     """
-    parser = argparse.ArgumentParser(prog="epictope",
-        description= 'Runs the Epictope pipeline on the provided protein accession')
+    parser = EpictopeParser(prog="epictope",
+        description= 'Runs the Epictope pipeline on the provided protein accession', 
+        formatter_class=argparse.RawTextHelpFormatter)
     
     subparsers = parser.add_subparsers(
         title="commands",
@@ -15,6 +24,6 @@ def main() -> None:
     from .commands.plot import add_parser as add_plot
     add_score(subparsers)
     add_plot(subparsers)
-    
+
     args = parser.parse_args()
     args.func(args)
--- src/epictope/commands/score.py
+++ src/epictope/commands/score.py
@@ -1,15 +1,21 @@
-from argparse import ArgumentParser
+import argparse
 from epictope.single_score import single_score
-def add_parser(subparsers:ArgumentParser):
-    parser = subparsers.add_parser("score", help="Calculate the scores for epictope site insertion for a protein from Uniprot")
+def add_parser(subparsers:argparse.ArgumentParser):
+    parser = subparsers.add_parser("score", help="Calculate the scores for epictope site insertion for a protein from Uniprot", 
+                                   formatter_class=argparse.RawTextHelpFormatter)
     
     parser.add_argument(dest="query", help="Uniprot accession of query sequence", type=str)
+
     parser.add_argument("-p", "--plot", dest="plot", action="store_true", required = False, 
-                    help = "boolean value for if the "
+                    help = "flag for whether the outputted score file should be plotted"
                     )
 
     parser.add_argument("-c", "--config", dest="config", required = False, 
-                    help = "path to config.yml file with parameter values",
+                    help = """path to config.yml file with parameter values for:
+  'species': list of species to use as blast databases more msa and shannon entropy calculation
+  'weights': dictionary of score component weights for shannon entropy 'h_weight', relative solvent accessibility 'rsa_weight', secondary structure 'ss_weight', and disordered binding region 'br_weight'
+  'ss_key': dictionary with the secondary structure mapping for score of secondary structure characters [GHIECTBSP-] from dssp
+  'max_sasa': dictionary mapping the maximum solvent accessibility for each amino acid""",
                     default = None)
 
     custom_struct_group = parser.add_argument_group(title='custom_structure', description='Arguments for custom structure input')
@@ -19,7 +25,7 @@ def add_parser(subparsers:ArgumentParser):
                     default = None,)
 
     custom_struct_group.add_argument("-r", "--residue", dest="res_start", required=False,
-                    help="starting amino acid position in structure file, 1 indexed",
+                    help="starting amino acid position in structure file (1 indexed)",
                     default=1, type=int)
     
     parser.set_defaults(func=run)
