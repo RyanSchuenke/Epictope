c8dc27457915750a53efb05ea93beab47fca7e6a
RyanSchuenke <ryan.schuenke17@gmail.com>
2026-02-26 15:24:41 -0600

    add option to plot score components

--- src/epictope/commands/plot.py
+++ src/epictope/commands/plot.py
@@ -1,11 +1,14 @@
 from argparse import ArgumentParser
-from epictope.plot_scores import plot_scores
+from epictope.plot_scores import plot_scores, plot_score_components
 
 def add_parser(subparsers:ArgumentParser):
-    parser = subparsers.add_parser("plot", help="Plot the score file output from `epictope score`")
+    parser = subparsers.add_parser("plot", help="Plot the moving average 'min' score from a score file output produced by `epictope score`")
     
     parser.add_argument(dest="score_file", help="Score file csv produced by `epictope score`", type=str)
 
+    parser.add_argument("-c", "--components", dest="components", action="store_true", required = False, 
+                    help = "plot the moving averages of the individual score components rather than the default minimum score",
+                    default = False,)
 
     parser.add_argument("-o", "--output", dest="output_file", required = False, 
                     help = "output file path for plotted score",
@@ -14,4 +17,7 @@ def add_parser(subparsers:ArgumentParser):
     parser.set_defaults(func=run)
 
 def run(args) -> None:
-    plot_scores(scores_file=args.score_file, output_file=args.output_file)
+    if args.components:
+        plot_score_components(scores_file=args.score_file, output_file=args.output_file)
+    else:
+        plot_scores(scores_file=args.score_file, output_file=args.output_file)
